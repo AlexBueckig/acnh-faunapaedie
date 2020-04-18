691bc64cd57a25997952d99710b697508fb72692
Alexander Bückig <AlexBueckig@gmail.com>
2020-04-18 13:21:02 +0200

    updated libraries

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+
+beforeEach(() => {
+  jest.spyOn(window, "fetch").mockImplementation((...args) => {
+    console.warn("window.fetch is not mocked for this call");
+    return Promise.reject(new Error("This must be mocked"));
+  });
+});
+
+afterEach(() => {
+  window.fetch.mockRestore();
+});
