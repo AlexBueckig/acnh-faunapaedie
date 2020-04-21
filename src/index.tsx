9690f37ef6e9072973e118a9bde4f70cd2879330
Alexander Bückig <AlexBueckig@gmail.com>
2020-04-21 18:09:23 +0200

    Added stuff

--- src/index.tsx
+++ src/index.tsx
@@ -21,3 +21,18 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+/* Possible fix for cache not updating
+  serviceWorker.register({
+  onUpdate: async (registration) => {
+    // We want to run this code only if we detect a new service worker is
+    // waiting to be activated.
+    // Details about it: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      // Once the service worker is unregistered, we can reload the page to let
+      // the browser download a fresh copy of our app (invalidating the cache)
+      window.location.reload();
+    }
+  },
+});
+*/
